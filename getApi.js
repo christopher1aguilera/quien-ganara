06f80953873e892532424c0d9f55027058b15325
christopheraguilera <christopher.aguilera.mad@gmail.com>
2020-12-16 10:49:20 -0300

    v.1

---
+++ getApi.js
@@ -0,0 +1,24 @@
+const { default: Axios } = require("axios")
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
+
+const getApi = () =>{
+    return Axios.get('https://randomuser.me/api')
+         .then(response => response.data.results[0])
+}
+
+function getusu(correos, usuarios, datausuarioJSON, res) {
+    getApi().then(datos =>{
+        let nombre = `${datos.name.title} ${datos.name.first} ${datos.name.last}`
+        let correo = `${datos.email}`
+        correos = `${correos},${correo}`
+        let pais = `${datos.location.country}`
+        let imagen = `${datos.picture.thumbnail}`
+        let ID = uuidv4().slice(0, 6)
+        let info = {nombre: nombre, foto: imagen, pais: pais, correo: correo, id: ID}
+        usuarios.push(info);
+        fs.writeFileSync("usuarios.json",JSON.stringify(datausuarioJSON));
+        res.end();
+    });        
+}
+module.exports = getusu
---
+++ index.js
@@ -0,0 +1,54 @@
+const getusu = require('./getApi')
+const enviar = require('./mailer')
+const http = require('http')
+const fs = require('fs')
+http
+.createServer(function (req, res) {
+let datausuarioJSON = JSON.parse(fs.readFileSync("usuarios.json", "utf8"));
+let datapremioJSON = JSON.parse(fs.readFileSync("premio.json", "utf8"));
+let usuarios = datausuarioJSON.usuarios;
+let correos = []
+correos.push('christopher.aguilera.mad@gmail.com')
+if (req.url.startsWith('/') && req.method == "GET") {
+res.setHeader('content-type', 'text/html')
+fs.readFile('index.html', 'utf8', (err, data) => {
+res.end(data)
+})
+}
+if ((req.url.startsWith('/usuario')) && req.method == "POST") {
+    //asincrona
+    getusu(correos, usuarios, datausuarioJSON, res)      
+}
+if (req.url.startsWith('/usuarios') && req.method == "GET") {
+res.end(JSON.stringify(datausuarioJSON));
+}
+if (req.url.startsWith('/premio') && req.method == "GET") {
+res.end(JSON.stringify(datapremioJSON));
+}
+if (req.url.startsWith('/premio') && req.method == "PUT") {
+    let body;
+    req.on("data", (payload) => {
+    body = JSON.parse(payload);
+        });
+    req.on("end", () => {
+        fs.writeFileSync("premio.json", JSON.stringify(body));
+        res.end();
+    });
+}
+if (req.url.startsWith('/ganador') && req.method == "GET") {
+ if (usuarios.length > 0){
+usuarios.forEach(element => {
+    correos.push(element.correo)
+});
+const random = Math.floor(Math.random() * (usuarios.length));
+let ganador = usuarios[random]
+console.log(random)
+var myString = JSON.stringify(ganador);
+res.writeHead(200, { 'Content-Type': 'application/json' })
+let mensaje = `El ganador de ¿Quien ganara? fue ${ganador.nombre}, gracias a todos por participar`
+enviar(correos, mensaje)
+res.end(myString)
+ }
+}
+})
+.listen(3000, () => console.log('Escuchando el puerto 3000'))
